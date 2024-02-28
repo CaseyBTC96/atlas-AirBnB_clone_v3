d2d5a69bb9cab6491da16564e1428696e6bdd86d
Casey Paul <casey.paul@atlasschool.com>
2024-02-28 01:11:55 +0000

    fixing  indention on file storage'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,8 +61,8 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-         """Delete a given object from __objects, if it exists."""
-         try:
+        """Delete a given object from __objects, if it exists."""
+        try:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
