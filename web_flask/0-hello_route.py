2120a05eb58a9d9569408d92e8c4b457e2ba27fa
casey paul <casey.paul@atlasschool.com>
2024-03-13 02:15:03 +0000

    E305 error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 """ 0. Script to start a Flask web application """
 
 
@@ -14,6 +12,7 @@ app = Flask(__name__)
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
