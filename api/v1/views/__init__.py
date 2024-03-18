3745cef9021f7ca95a4c1e3e009b14a9b4b854b0
casey paul <casey.paul@atlasschool.com>
2024-03-18 23:11:00 +0000

    task 4

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon March 18 18:07:30
+@author: Casey Paul
+         Crystal
+"""
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+if (__name__ == 'api.v1.views'):
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
