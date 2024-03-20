917acbecba73f19d3a8c5e4e72eed70a057a7a35
Casey Paul <Casey.Paul@atlasschool.com>
2024-03-20 16:59:12 +0000

    bug fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,7 @@ class FileStorage:
                 cls = eval(cls)
             cls_dict = {}
             for k, v in self.__objects.items():
-                if isinstance(v , cls):
+                if isinstance(v, cls):
                     cls_dict[k] = v
             return cls_dict
         return self.__objects
